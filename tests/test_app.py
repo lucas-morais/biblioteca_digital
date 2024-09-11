3455065a5e3e47e2597a94d8070d3184b1326f45
lucas-morais <lucasmoraisss@gmail.com>
2024-09-11 14:42:25 -0300

    feat: testing user creation

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,27 @@ from biblioteca_digital.app import app
 
 def test_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)
-    response = client.get("/")
+    response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "Olá Mundo!"}
+    assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users',
+        json={
+            'username': 'ringo',
+            'email': 'ringo@email.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'ringo',
+        'email': 'ringo@email.com',
+        'id': 1,
+    }
