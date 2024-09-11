968b48b8d2e642ccadd0684a55be19c343ad74d7
lucas-morais <lucasmoraisss@gmail.com>
2024-09-11 14:42:06 -0300

    feat: route create user

--- biblioteca_digital/app.py
+++ biblioteca_digital/app.py
@@ -1,8 +1,24 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from biblioteca_digital.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+database = []
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
