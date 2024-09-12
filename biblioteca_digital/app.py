6a40af864d69f97a33dd552f1f175a0bc40e1748
lucas-morais <lucasmoraisss@gmail.com>
2024-09-12 07:35:41 -0300

    feat: get user by id

--- biblioteca_digital/app.py
+++ biblioteca_digital/app.py
@@ -35,6 +35,18 @@ def read_users():
     return {'users': database}
 
 
+@app.get(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def read_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user = database[user_id - 1]
+    return user
+
+
 @app.put(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
