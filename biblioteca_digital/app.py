c262cb288a9b08a07f95aea950af26bdb3818640
lucas-morais <lucasmoraisss@gmail.com>
2024-09-13 08:33:33 -0300

    feat: create user in db

--- biblioteca_digital/app.py
+++ biblioteca_digital/app.py
@@ -1,7 +1,11 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from biblioteca_digital.database import get_session
+from biblioteca_digital.models import User
 from biblioteca_digital.schemas import (
     Message,
     UserDB,
@@ -22,12 +26,31 @@ def read_root():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select((User.username == user.username) | (User.email == user.email))
+    )
 
-    database.append(user_with_id)
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail='Email already exists',
+                )
+            )
 
-    return user_with_id
+    db_user = User(**user.model_dump())
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
