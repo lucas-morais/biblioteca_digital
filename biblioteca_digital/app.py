9ca578ef9396d5e80e8283b2ff16ddd708a2bf12
lucas-morais <lucasmoraisss@gmail.com>
2024-09-13 09:08:36 -0300

    feat: delete user from db

--- biblioteca_digital/app.py
+++ biblioteca_digital/app.py
@@ -98,11 +98,14 @@ def update_user(
     status_code=HTTPStatus.OK,
     response_model=Message,
 )
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    del database[user_id - 1]
 
+    session.delete(db_user)
+    session.commit()
     return {'message': 'User deleted'}
