0912c2540199c66cb053cea7236b467f487dc5fe
lucas-morais <lucasmoraisss@gmail.com>
2024-09-14 19:46:41 -0300

    feat: generate and verify token and hashed password

---
+++ biblioteca_digital/security.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+
+from jwt import encode
+from pwdlib import PasswordHash
+from zoneinfo import ZoneInfo
+
+SECRET_KEY = 'your-secret-key'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRES = 30
+pwd_context = PasswordHash.recommended()
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRES
+    )
+
+    to_encode.update({'exp': expire})
+    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_passowrd: str, hashed_password: str):
+    return pwd_context.verify(plain_passowrd, hashed_password)
